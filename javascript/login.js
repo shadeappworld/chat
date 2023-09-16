8764bb70b0420af376f14ea97f8f8a015c94de8c
Shade Software <121112889+shadeappworld@users.noreply.github.com>
2023-09-16 17:56:52 +0200

    Add files via upload

--- javascript/login.js
+++ javascript/login.js
@@ -1,4 +1,4 @@
-const form = document.querySelector(".signup form"),
+const form = document.querySelector(".login form"),
 continueBtn = form.querySelector(".button input"),
 errorText = form.querySelector(".error-text");
 
@@ -8,13 +8,13 @@ form.onsubmit = (e)=>{
 
 continueBtn.onclick = ()=>{
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "php/signup.php", true);
+    xhr.open("POST", "php/login.php", true);
     xhr.onload = ()=>{
       if(xhr.readyState === XMLHttpRequest.DONE){
           if(xhr.status === 200){
               let data = xhr.response;
               if(data === "success"){
-                location.href="users.php";
+                location.href = "users.php";
               }else{
                 errorText.style.display = "block";
                 errorText.textContent = data;
